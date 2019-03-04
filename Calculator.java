e847b3a85f4265c397b60960054579a33cdb7e18
Sana <guru-prasad.sana@capgemini.com>
2019-03-04 12:36:18 +0530

    modified calculator.java file

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,6 @@
 public class Calculator{
               public static void main(String[] args){
                   System.out.println("Hello World!!!");
+                  System.out.println("calculator java program");
                }
 }
